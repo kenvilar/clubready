6ef9b6d86842378a71d65ca2845205a818c3ecef
Ken Vilar <kenvilar@gmail.com>
2020-02-06 15:33:09 +0800

    Add some columns for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,10 +16,28 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 60);
+
+            $table->text('address')->nullable();
+            $table->string('suburb')->nullable();
+            $table->string('state')->nullable();
+            $table->string('postcode')->nullable();
+            $table->string('country')->nullable();
+            $table->string('phone_home')->nullable();
+            $table->string('phone_mobile')->nullable();
+            $table->string('alternative_email')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->integer('member_number')->unique();
+            $table->integer('club_racenumber')->unique();
+            $table->string('verified')->default(User::UNVERIFIED_USER);
+            $table->string('verification_token')->nullable();
+            $table->string('admin')->default(User::NON_ADMIN_USER);
+            $table->string('active')->default(User::ACTIVE_USER);
+
             $table->rememberToken();
             $table->timestamps();
         });
