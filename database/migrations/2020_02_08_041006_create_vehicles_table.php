d89589817fc389f86e2fc2f989a6d17cfa3b12e2
Ken Vilar <kenvilar@gmail.com>
2020-02-10 15:03:15 +0800

    Generate Vehicle model and migration

---
+++ database/migrations/2020_02_08_041006_create_vehicles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('make');
+            $table->string('model');
+            $table->unsignedInteger('year');
+            $table->unsignedInteger('capacity');
+            $table->string('induction');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
