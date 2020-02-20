4107ad631cadeffaebb873216aaf38bfd625954c
Ken Vilar <kenvilar@gmail.com>
2020-02-20 11:49:29 +0800

    Add showMessage() function

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -26,8 +26,13 @@ trait ApiResponser
         return $this->successResponse($collection, $code);
     }
 
-    protected function showOne(Model $instance, $code)
+    protected function showOne(Model $instance, $code = 200)
     {
         return $this->successResponse($instance, $code);
+    }
+
+    protected function showMessage($message, $code = 200)
+    {
+        return $this->successResponse(['data' => $message], $code);
     }
 }
