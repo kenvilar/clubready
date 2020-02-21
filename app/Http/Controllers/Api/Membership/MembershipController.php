db30794cee8308795f3fbe946557d690d856759e
Ken Vilar <kenvilar@gmail.com>
2020-02-21 14:14:27 +0800

    Implement Membership controller methods

--- app/Http/Controllers/Api/Membership/MembershipController.php
+++ app/Http/Controllers/Api/Membership/MembershipController.php
@@ -3,62 +3,98 @@
 namespace App\Http\Controllers\Api\Membership;
 
 use App\Http\Controllers\Controller;
+use App\Models\Membership;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class MembershipController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function index()
     {
-        //
+        $memberships = Membership::all();
+
+        return $this->showAll($memberships);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'year' => 'required|min:4|max:4',
+            'name' => 'required|min:2',
+            'amount' => 'required|min:1',
+        ];
+        $this->validate($request, $rules);
+        $data = $request->all();
+        $membership = Membership::query()->create($data);
+
+        return $this->showOne($membership, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Membership $membership
+     * @return JsonResponse
      */
-    public function show($id)
+    public function show(Membership $membership)
     {
-        //
+        return $this->showOne($membership);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Membership $membership
+     * @return JsonResponse
+     * @throws ValidationException
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Membership $membership)
     {
-        //
+        $rules = [
+            'year' => 'required|min:4|max:4',
+            'name' => 'required|min:2',
+            'amount' => 'required|min:1',
+        ];
+        $this->validate($request, $rules);
+
+        $membership->year = $request->year;
+        $membership->name = $request->name;
+        $membership->amount = $request->amount;
+
+        if (!$membership->isDirty()) {
+            return $this->errorResponse('You need to specify a different value to update', 422);
+        }
+
+        $membership->save();
+
+        return $this->showOne($membership);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Membership $membership
+     * @return JsonResponse
+     * @throws \Exception
      */
-    public function destroy($id)
+    public function destroy(Membership $membership)
     {
-        //
+        $membership->delete();
+
+        return $this->showOne($membership);
     }
 }
