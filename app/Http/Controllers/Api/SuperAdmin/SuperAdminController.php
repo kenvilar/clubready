a37f8c49a18f12bb9ade85d5ce1af2cdb75c505d
Ken Vilar <kenvilar@gmail.com>
2020-02-22 09:01:21 +0800

    Implement SuperAdmin controller methods

--- app/Http/Controllers/Api/SuperAdmin/SuperAdminController.php
+++ app/Http/Controllers/Api/SuperAdmin/SuperAdminController.php
@@ -3,62 +3,87 @@
 namespace App\Http\Controllers\Api\SuperAdmin;
 
 use App\Http\Controllers\Controller;
+use App\Models\SuperAdmin;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SuperAdminController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function index()
     {
-        //
+        $superAdmins = SuperAdmin::all();
+
+        return $this->showAll($superAdmins);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'user_id' => 'integer',
+        ];
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+        $superAdmin = SuperAdmin::query()->create($data);
+
+        return $this->showOne($superAdmin, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param SuperAdmin $superAdmin
+     * @return JsonResponse
      */
-    public function show($id)
+    public function show(SuperAdmin $superAdmin)
     {
-        //
+        return $this->showOne($superAdmin);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param SuperAdmin $superAdmin
+     * @return JsonResponse
+     * @throws ValidationException
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, SuperAdmin $superAdmin)
     {
-        //
+        $rules = [
+            'user_id' => 'integer',
+        ];
+        $this->validate($request, $rules);
+
+        $superAdmin->save();
+
+        return $this->showOne($superAdmin);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param SuperAdmin $superAdmin
+     * @return JsonResponse
+     * @throws \Exception
      */
-    public function destroy($id)
+    public function destroy(SuperAdmin $superAdmin)
     {
-        //
+        $superAdmin->delete();
+
+        return $this->showOne($superAdmin);
     }
 }
