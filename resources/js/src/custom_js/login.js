f7396ac969b3aaddac1b6c97a5f171a9051f7447
Ken Vilar <kenvilar@gmail.com>
2020-03-12 15:14:49 +0800

    Add login.js

---
+++ resources/js/src/custom_js/login.js
@@ -0,0 +1,26 @@
+"use strict";
+$(document).ready(function () {
+    //=================Preloader===========//
+    $(window).on('load', function () {
+        $('.preloader img').fadeOut();
+        $('.preloader').fadeOut();
+    });
+    //=================end of Preloader===========//
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_minimal-blue',
+        radioClass: 'iradio_minimal-blue',
+        increaseArea: '20%' // optional
+    });
+    $("#authentication").bootstrapValidator({
+        fields: {
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'The email address is required'
+                    },
+                }
+            },
+        }
+    });
+
+});
