63cc2962792f0baae0496f68d48a7b9eeacffc51
Ken Vilar <kenvilar@gmail.com>
2020-03-24 14:50:33 +0800

    Modify club-members html layouts

--- resources/views/admin/club-members/createAndEditForm.blade.php
+++ resources/views/admin/club-members/createAndEditForm.blade.php
@@ -1,7 +1,29 @@
 @extends('layouts.app')
 
-@section('content')
+@push('stylesheets')
+    <link href="{{asset('vendors/iCheck/css/all.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('css/form_layouts.css')}}" rel="stylesheet" type="text/css"/>
+@endpush
 
-    <club-members-create-and-edit-view-vue :model_id="{{isset($clubMember->id) ? $clubMember->id : 0}}"/>
-
+@section('breadcrumb')
+    <h1>Club Members</h1>
+    <ol class="breadcrumb">
+        <li>
+            <a href="{{route('home')}}"><i class="fa fa-fw ti-home"></i>Dashboard</a>
+        </li>
+        <li><a href="{{route('web.club-member.index')}}">Show List</a></li>
+        @if (isset($clubMember->id))
+            <li><a href="{{route('web.club-member.show', $clubMember->id)}}">Show Item</a></li>
+        @endif
+        <li class="active">@if (isset($clubMember->id)) Edit @else Create @endif Item</li>
+    </ol>
 @endsection
+
+@section('content')
+    <club-members-create-and-edit-view-vue :model_id="{{isset($clubMember->id) ? $clubMember->id : 0}}"/>
+@endsection
+
+@push('footer-scripts')
+    <script src="{{asset('vendors/iCheck/js/icheck.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('js/custom_js/form_layouts.js')}}" type="text/javascript"></script>
+@endpush
