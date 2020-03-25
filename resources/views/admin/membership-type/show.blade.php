8e2c1cff3f63dad622f6ee4fafb55cc836ce989c
Ken Vilar <kenvilar@gmail.com>
2020-03-25 11:59:06 +0800

    Enhace UI on membershipTypes blade templates

--- resources/views/admin/membership-type/show.blade.php
+++ resources/views/admin/membership-type/show.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.app')
 
-@section('content')
+@push('stylesheets')
+    <link href="{{ asset('css/custom_css/invoice.css') }}" rel="stylesheet" type="text/css">
+@endpush
 
-    <membership-types-detail-view-vue :model_id="{{isset($membershipType->id) ? $membershipType->id : 0}}"/>
-
+@section('breadcrumb')
+    <h1>Membership Types</h1>
+    <ol class="breadcrumb">
+        <li>
+            <a href="{{route('home')}}"><i class="fa fa-fw ti-home"></i>Dashboard</a>
+        </li>
+        <li><a href="{{route('web.membership-type.index')}}">Show List</a></li>
+        <li class="active">Show Item</li>
+    </ol>
 @endsection
+
+@section('content')
+    <membership-types-detail-view-vue :model_id="{{isset($membershipType->id) ? $membershipType->id : 0}}"/>
+@endsection
+
+@push('footer-scripts')
+    <script src="{{ asset('js/custom_js/invoice.js') }}" type="text/javascript"></script>
+@endpush
