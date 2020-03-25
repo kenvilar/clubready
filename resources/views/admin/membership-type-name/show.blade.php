030a60e5eac04f64ad141a16b8f91d9d07da2ef2
Ken Vilar <kenvilar@gmail.com>
2020-03-25 13:54:56 +0800

    Enhance UI for membership-types vue and blade templates

--- resources/views/admin/membership-type-name/show.blade.php
+++ resources/views/admin/membership-type-name/show.blade.php
@@ -1,8 +1,25 @@
 @extends('layouts.app')
 
-@section('content')
+@push('stylesheets')
+    <link href="{{ asset('css/custom_css/invoice.css') }}" rel="stylesheet" type="text/css">
+@endpush
 
+@section('breadcrumb')
+    <h1>Membership Type Names</h1>
+    <ol class="breadcrumb">
+        <li>
+            <a href="{{route('home')}}"><i class="fa fa-fw ti-home"></i>Dashboard</a>
+        </li>
+        <li><a href="{{route('web.membership-type-name.index')}}">Show List</a></li>
+        <li class="active">Show Item</li>
+    </ol>
+@endsection
+
+@section('content')
     <membership-type-names-detail-view-vue
         :model_id="{{isset($membershipTypeName->id) ? $membershipTypeName->id : 0}}"/>
-
 @endsection
+
+@push('footer-scripts')
+    <script src="{{ asset('js/custom_js/invoice.js') }}" type="text/javascript"></script>
+@endpush
