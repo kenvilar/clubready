2fd2d4488a5633eb56ad8904c7ec69c980350537
Ken Vilar <kenvilar@gmail.com>
2020-04-07 12:45:39 +0800

    Register passport vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -74,6 +74,13 @@ const vehicle_files = require.context('./components/admin/vehicles/', true, /\.v
 vehicle_files.keys().map(key => Vue.component(vehicle_files(key).default.name, vehicle_files(key).default));
 
 /**
+ * Laravel Passport Vue Components
+ */
+Vue.component('passport-clients', require('./components/passport/Clients.vue').default);
+Vue.component('passport-authorized-clients', require('./components/passport/AuthorizedClients.vue').default);
+Vue.component('passport-personal-access-tokens', require('./components/passport/PersonalAccessTokens.vue').default);
+
+/**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
