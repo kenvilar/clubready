8aa12171d8d6dc3db4c4f739ea119aee926fbb6f
Ken Vilar <kenvilar@gmail.com>
2020-04-07 18:37:31 +0800

    Enable the implicit grant type

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -39,5 +39,7 @@ class AuthServiceProvider extends ServiceProvider
         Passport::tokensExpireIn(now()->addDays(15));
         Passport::refreshTokensExpireIn(now()->addDays(30));
         Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+
+        Passport::enableImplicitGrant();
     }
 }
