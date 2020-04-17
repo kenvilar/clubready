ea8fe2421a03b64ff8f0f1d356eb6f341ccc9afd
Ken Vilar <kenvilar@gmail.com>
2020-04-17 14:51:58 +0800

    Remove data key

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -20,7 +20,7 @@ trait ApiResponser
     protected function showAll(Collection $collection, $code = 200)
     {
         if ($collection->isEmpty()) {
-            return $this->successResponse(['data' => $collection], $code);
+            return $this->successResponse($collection, $code);
         }
 
         return $this->successResponse($collection, $code);
