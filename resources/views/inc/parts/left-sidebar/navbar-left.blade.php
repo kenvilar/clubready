4c972d8f0d3ba17971474b53c5a1d21254dfece3
Ken Vilar <kenvilar@gmail.com>
2020-04-27 07:07:39 +0800

    Make left sidebar menus accessible to auth()->user()->admin

--- resources/views/inc/parts/left-sidebar/navbar-left.blade.php
+++ resources/views/inc/parts/left-sidebar/navbar-left.blade.php
@@ -1,5 +1,5 @@
-<ul class="navigation">
-    @if (Auth::check())
+@if (Auth::check())
+    <ul class="navigation">
         @if (auth()->user()->admin == 1)
             <li class="{{request()->routeIs('web.passport.*') ? 'active' : ''}} menu-dropdown" id="active">
                 <a>
@@ -25,77 +25,81 @@
                     </li>
                 </ul>
             </li>
+            <li class="{{request()->routeIs('web.user.*') ? 'active' : ''}} menu-dropdown" id="active">
+                <a href="{{ route('web.user.index') }}">
+                    <i class="menu-icon ti-user"></i>
+                    <span class="mm-text ">Users</span>
+                    <span class="fa arrow"></span>
+                </a>
+                <ul class="sub-menu">
+                    <li class="{{request()->routeIs('web.user.index') ? 'active' : ''}}">
+                        <a href="{{route('web.user.index')}}">
+                            <i class="menu-icon ti-user"></i> Verified Users
+                        </a>
+                    </li>
+                    <li class="{{request()->routeIs('web.user.index') ? 'active' : ''}}">
+                        <a href="{{route('web.user.index')}}">
+                            <i class="menu-icon ti-user"></i> Unverified Users
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="{{request()->routeIs('web.club.*') ? 'active' : ''}}">
+                <a href="{{ route('web.club.index') }}">
+                    <i class="menu-icon ti-desktop"></i>
+                    <span class="mm-text ">Clubs</span>
+                </a>
+            </li>
         @endif
-    @endif
 
-    <li class="{{request()->routeIs('web.user.*') ? 'active' : ''}} menu-dropdown" id="active">
-        <a href="{{ route('web.user.index') }}">
-            <i class="menu-icon ti-user"></i>
-            <span class="mm-text ">Users</span>
-            <span class="fa arrow"></span>
-        </a>
-        <ul class="sub-menu">
-            <li class="{{request()->routeIs('web.user.index') ? 'active' : ''}}">
-                <a href="{{route('web.user.index')}}">
-                    <i class="menu-icon ti-user"></i> Verified Users
+        <li class="{{request()->routeIs('web.club-member.*') ? 'active' : ''}}">
+            <a href="{{ route('web.club-member.index') }}">
+                <i class="menu-icon icon-people icons"></i>
+                <span class="mm-text ">Club Members</span>
+            </a>
+        </li>
+        <li class="{{request()->routeIs('web.induction.*') ? 'active' : ''}}">
+            <a href="{{ route('web.induction.index') }}">
+                <i class="menu-icon fa fa-fw fa-tachometer"></i>
+                <span class="mm-text ">Inductions</span>
+            </a>
+        </li>
+        <li class="{{request()->routeIs('web.membership.*') ? 'active' : ''}}">
+            <a href="{{ route('web.membership.index') }}">
+                <i class="menu-icon ti-tag"></i>
+                <span class="mm-text ">Memberships</span>
+            </a>
+        </li>
+        <li class="{{request()->routeIs('web.membership-type.*') ? 'active' : ''}}">
+            <a href="{{ route('web.membership-type.index') }}">
+                <i class="menu-icon ti-bookmark-alt"></i>
+                <span class="mm-text ">Membership Types</span>
+            </a>
+        </li>
+        <li class="{{request()->routeIs('web.membership-type-name.*') ? 'active' : ''}}">
+            <a href="{{ route('web.membership-type-name.index') }}">
+                <i class="menu-icon ti-id-badge"></i>
+                <span class="mm-text ">Membership Type Names</span>
+            </a>
+        </li>
+
+        @if (auth()->user()->admin == 1)
+            <li class="{{request()->routeIs('web.super-admin.*') ? 'active' : ''}}">
+                <a href="{{ route('web.super-admin.index') }}">
+                    <i class="menu-icon fa fa-fw fa-users"></i>
+                    <span class="mm-text ">Super Admins</span>
                 </a>
             </li>
-            <li class="{{request()->routeIs('web.user.index') ? 'active' : ''}}">
-                <a href="{{route('web.user.index')}}">
-                    <i class="menu-icon ti-user"></i> Unverified Users
-                </a>
-            </li>
-        </ul>
-    </li>
-    <li class="{{request()->routeIs('web.club.*') ? 'active' : ''}}">
-        <a href="{{ route('web.club.index') }}">
-            <i class="menu-icon ti-desktop"></i>
-            <span class="mm-text ">Clubs</span>
-        </a>
-    </li>
-    <li class="{{request()->routeIs('web.club-member.*') ? 'active' : ''}}">
-        <a href="{{ route('web.club-member.index') }}">
-            <i class="menu-icon icon-people icons"></i>
-            <span class="mm-text ">Club Members</span>
-        </a>
-    </li>
-    <li class="{{request()->routeIs('web.induction.*') ? 'active' : ''}}">
-        <a href="{{ route('web.induction.index') }}">
-            <i class="menu-icon fa fa-fw fa-tachometer"></i>
-            <span class="mm-text ">Inductions</span>
-        </a>
-    </li>
-    <li class="{{request()->routeIs('web.membership.*') ? 'active' : ''}}">
-        <a href="{{ route('web.membership.index') }}">
-            <i class="menu-icon ti-tag"></i>
-            <span class="mm-text ">Memberships</span>
-        </a>
-    </li>
-    <li class="{{request()->routeIs('web.membership-type.*') ? 'active' : ''}}">
-        <a href="{{ route('web.membership-type.index') }}">
-            <i class="menu-icon ti-bookmark-alt"></i>
-            <span class="mm-text ">Membership Types</span>
-        </a>
-    </li>
-    <li class="{{request()->routeIs('web.membership-type-name.*') ? 'active' : ''}}">
-        <a href="{{ route('web.membership-type-name.index') }}">
-            <i class="menu-icon ti-id-badge"></i>
-            <span class="mm-text ">Membership Type Names</span>
-        </a>
-    </li>
-    <li class="{{request()->routeIs('web.super-admin.*') ? 'active' : ''}}">
-        <a href="{{ route('web.super-admin.index') }}">
-            <i class="menu-icon fa fa-fw fa-users"></i>
-            <span class="mm-text ">Super Admins</span>
-        </a>
-    </li>
-    <li class="{{request()->routeIs('web.vehicle.*') ? 'active' : ''}}">
-        <a href="{{ route('web.vehicle.index') }}">
-            <i class="menu-icon ti-car"></i>
-            <span class="mm-text ">Vehicles</span>
-        </a>
-    </li>
-</ul>
+        @endif
+
+        <li class="{{request()->routeIs('web.vehicle.*') ? 'active' : ''}}">
+            <a href="{{ route('web.vehicle.index') }}">
+                <i class="menu-icon ti-car"></i>
+                <span class="mm-text ">Vehicles</span>
+            </a>
+        </li>
+    </ul>
+@endif
 
 @push('footer-scripts')
     <link rel="stylesheet" type="text/css" href="{{asset('vendors/simple-line-icons/css/simple-line-icons.css')}}">
