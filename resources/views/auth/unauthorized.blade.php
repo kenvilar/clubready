5e8f58437743e6da92872974a698415cc1282438
Ken Vilar <kenvilar@gmail.com>
2020-04-28 23:56:06 +0800

    Add if statement for $role

--- resources/views/auth/unauthorized.blade.php
+++ resources/views/auth/unauthorized.blade.php
@@ -8,7 +8,12 @@
                 <div class="card">
                     <div class="card-body">
                         <div class="card-title text-center">
-                            <h6>You cannot access this page! This is for only '{{$role}}'.</h6>
+                            <h6>
+                                You cannot access this page!
+                                @if ($role)
+                                    This is for only '{{$role}}'.
+                                @endif
+                            </h6>
                         </div>
                     </div>
                 </div>
