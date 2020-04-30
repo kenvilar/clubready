d972652a776021f278be1c358243eaa1d0c7fc0f
Ken Vilar <kenvilar@gmail.com>
2020-04-30 23:05:17 +0800

    Add web ClubMemberVehicleController

---
+++ app/Http/Controllers/Web/ClubMember/ClubMemberVehicleController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Web\ClubMember;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ClubMemberVehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+}
