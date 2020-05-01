06d1865e38445aaa6b7341a7e133137fa954471f
Ken Vilar <kenvilar@gmail.com>
2020-05-01 11:54:20 +0800

    Implement initial blade templates for ClubMemberVehicleController

--- app/Http/Controllers/Web/ClubMember/ClubMemberVehicleController.php
+++ app/Http/Controllers/Web/ClubMember/ClubMemberVehicleController.php
@@ -3,49 +3,63 @@
 namespace App\Http\Controllers\Web\ClubMember;
 
 use App\Http\Controllers\Controller;
+use App\Models\ClubMember;
+use App\Models\Vehicle;
+use App\User;
 use Illuminate\Http\Request;
 
 class ClubMemberVehicleController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('strict-user');
+    }
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param ClubMember $clubMember
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function index()
+    public function index(ClubMember $clubMember)
     {
-        //
+        return view('admin.club-members.vehicles.index', ['clubMember' => $clubMember]);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param ClubMember $clubMember
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function create()
+    public function create(ClubMember $clubMember)
     {
-        //
+        return view('admin.club-members.vehicles.createAndEditForm', ['clubMember' => $clubMember]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param int $id
-     * @return \Illuminate\Http\Response
+     * @param ClubMember $clubMember
+     * @param Vehicle $vehicle
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function show($id)
+    public function show(ClubMember $clubMember, Vehicle $vehicle)
     {
-        //
+        return view('admin.club-members.vehicles.show', ['clubMember' => $clubMember, 'vehicle' => $vehicle]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param int $id
-     * @return \Illuminate\Http\Response
+     * @param ClubMember $clubMember
+     * @param Vehicle $vehicle
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function edit($id)
+    public function edit(ClubMember $clubMember, Vehicle $vehicle)
     {
-        //
+        return view('admin.club-members.vehicles.createAndEditForm',
+            ['clubMember' => $clubMember, 'vehicle' => $vehicle]);
     }
 }
