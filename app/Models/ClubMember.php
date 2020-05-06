5fd37ba166cff81eebd75635161605b3d74c42c0
Ken Vilar <kenvilar@gmail.com>
2020-05-07 00:31:23 +0800

    Add club_id at the last in club_member uuid

--- app/Models/ClubMember.php
+++ app/Models/ClubMember.php
@@ -30,7 +30,7 @@ class ClubMember extends Model
         parent::boot();
 
         static::creating(function ($clubMember) {
-            $clubMember->uuid = (User::query()->find($clubMember->user_id))->member_number;
+            $clubMember->uuid = (User::query()->find($clubMember->user_id))->member_number . '-' . $clubMember->club_id;
         });
     }
 
