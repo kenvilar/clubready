c4ed4e63a350d6bd6da94b1d45b7c4566f629bb4
Ken Vilar <kenvilar@gmail.com>
2020-05-06 22:17:13 +0800

    Add middlewares

--- app/Http/Controllers/Web/ClubMember/ClubMemberMembershipTypeController.php
+++ app/Http/Controllers/Web/ClubMember/ClubMemberMembershipTypeController.php
@@ -9,6 +9,17 @@ use App\Models\MembershipType;
 class ClubMemberMembershipTypeController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('strict-user');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @param ClubMember $clubMember
