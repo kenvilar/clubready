269ddf2eb70f6df3376928f487c161a1ecd88fbb
Ken Vilar <kenvilar@gmail.com>
2020-05-08 20:55:19 +0800

    Modify members-choose-club.blade.php

--- resources/views/auth/members-choose-club.blade.php
+++ resources/views/auth/members-choose-club.blade.php
@@ -1,16 +1,14 @@
-@extends('layouts.auth')
+@extends('layouts.app')
 
 @section('content')
 
-    <section class="content">
+    <section class="content mt-5">
         <div class="row">
             <div class="col-lg-12">
                 <div class="card">
                     <div class="card-body">
                         <div class="card-title text-center">
-                            <h6>
-                                PLEASE CHOOSE A CLUB!!
-                            </h6>
+                            <h6>PLEASE CHOOSE A CLUB!!</h6>
                         </div>
                     </div>
                 </div>
@@ -21,5 +19,9 @@
 @endsection
 
 @push('footer-scripts')
-    <script src="{{ asset('js/custom_js/login.js') }}" type="text/javascript"></script>
+    <script>
+        $("img[alt='Logo'], .left-side, .right-side .content-header, " +
+            ".user.user-menu li:nth-child(2), .user.user-menu li:nth-child(4), .navbar-btn.sidebar-toggle").hide();
+        $(".right-side").css({"margin-left": 0});
+    </script>
 @endpush
