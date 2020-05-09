80b023b853f93dbbb7818760710ea3a159fd18ea
Ken Vilar <kenvilar@gmail.com>
2020-05-09 18:41:15 +0800

    Fix left sidebar menu routes

--- resources/views/inc/parts/left-sidebar/navbar-left-for-clubmembers.blade.php
+++ resources/views/inc/parts/left-sidebar/navbar-left-for-clubmembers.blade.php
@@ -7,25 +7,29 @@
     </li>
 @endif
 <li class="{{request()->routeIs('web.club-member.memberships.*') ? 'active' : ''}}">
-    <a href="{{ route('web.club-member.memberships.index', auth()->user()->member_number) }}">
+    <a href="{{ route('web.club-member.memberships.index',
+                auth()->user()->member_number . '-' . session('my_app__member__club_id')) }}">
         <i class="menu-icon ti-tag"></i>
         <span class="mm-text ">Memberships</span>
     </a>
 </li>
 <li class="{{request()->routeIs('web.club-member.membership-types.*') ? 'active' : ''}}">
-    <a href="{{ route('web.club-member.membership-types.index', auth()->user()->member_number) }}">
+    <a href="{{ route('web.club-member.membership-types.index',
+                auth()->user()->member_number . '-' . session('my_app__member__club_id')) }}">
         <i class="menu-icon ti-bookmark-alt"></i>
         <span class="mm-text ">Membership Types</span>
     </a>
 </li>
 <li class="{{request()->routeIs('web.club-member.membership-type-names.*') ? 'active' : ''}}">
-    <a href="{{ route('web.club-member.membership-type-names.index', auth()->user()->member_number) }}">
+    <a href="{{ route('web.club-member.membership-type-names.index',
+                auth()->user()->member_number . '-' . session('my_app__member__club_id')) }}">
         <i class="menu-icon ti-id-badge"></i>
         <span class="mm-text ">Membership Type Names</span>
     </a>
 </li>
 <li class="{{request()->routeIs('web.club-member.vehicles.*') ? 'active' : ''}}">
-    <a href="{{ route('web.club-member.vehicles.index', auth()->user()->member_number) }}">
+    <a href="{{ route('web.club-member.vehicles.index',
+                auth()->user()->member_number . '-' . session('my_app__member__club_id')) }}">
         <i class="menu-icon ti-car"></i>
         <span class="mm-text ">Vehicles</span>
     </a>
