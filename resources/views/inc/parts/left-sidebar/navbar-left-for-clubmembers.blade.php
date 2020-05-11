3a016c356391e93332276ae79d072a83dac9d32a
Ken Vilar <kenvilar@gmail.com>
2020-05-11 17:39:12 +0800

    Fix routes params

--- resources/views/inc/parts/left-sidebar/navbar-left-for-clubmembers.blade.php
+++ resources/views/inc/parts/left-sidebar/navbar-left-for-clubmembers.blade.php
@@ -6,30 +6,34 @@
         </a>
     </li>
 @endif
+@if (env('APP_DEBUG'))
+    <li class="{{request()->routeIs('getAllSessionData') ? 'active' : ''}}">
+        <a href="{{ route('getAllSessionData') }}" target="_blank">
+            <i class="menu-icon ti-tag"></i>
+            <span class="mm-text ">All Session Data</span>
+        </a>
+    </li>
+@endif
 <li class="{{request()->routeIs('web.club-member.memberships.*') ? 'active' : ''}}">
-    <a href="{{ route('web.club-member.memberships.index',
-                auth()->user()->member_number . '-' . session('my_app__member__club_id')) }}">
+    <a href="{{ route('web.club-member.memberships.index', session('my_app__current_member')['uuid']) }}">
         <i class="menu-icon ti-tag"></i>
         <span class="mm-text ">Memberships</span>
     </a>
 </li>
 <li class="{{request()->routeIs('web.club-member.membership-types.*') ? 'active' : ''}}">
-    <a href="{{ route('web.club-member.membership-types.index',
-                auth()->user()->member_number . '-' . session('my_app__member__club_id')) }}">
+    <a href="{{ route('web.club-member.membership-types.index', session('my_app__current_member')['uuid']) }}">
         <i class="menu-icon ti-bookmark-alt"></i>
         <span class="mm-text ">Membership Types</span>
     </a>
 </li>
 <li class="{{request()->routeIs('web.club-member.membership-type-names.*') ? 'active' : ''}}">
-    <a href="{{ route('web.club-member.membership-type-names.index',
-                auth()->user()->member_number . '-' . session('my_app__member__club_id')) }}">
+    <a href="{{ route('web.club-member.membership-type-names.index', session('my_app__current_member')['uuid']) }}">
         <i class="menu-icon ti-id-badge"></i>
         <span class="mm-text ">Membership Type Names</span>
     </a>
 </li>
 <li class="{{request()->routeIs('web.club-member.vehicles.*') ? 'active' : ''}}">
-    <a href="{{ route('web.club-member.vehicles.index',
-                auth()->user()->member_number . '-' . session('my_app__member__club_id')) }}">
+    <a href="{{ route('web.club-member.vehicles.index', session('my_app__current_member')['uuid']) }}">
         <i class="menu-icon ti-car"></i>
         <span class="mm-text ">Vehicles</span>
     </a>
