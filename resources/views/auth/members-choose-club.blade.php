f9bfd0ced5e457497584dd880662700cbba2049f
Ken Vilar <kenvilar@gmail.com>
2020-05-11 17:40:03 +0800

    Add input fields for postForm() function

--- resources/views/auth/members-choose-club.blade.php
+++ resources/views/auth/members-choose-club.blade.php
@@ -11,7 +11,7 @@
                             <h6>PLEASE CHOOSE A CLUB!!</h6>
                             <div class="w-50 m-auto">
                                 @foreach($clubs as $club)
-                                    <a href="" onclick="postForm({{ $club['club']['id'] }});event.preventDefault();">
+                                    <a href="" onclick="postForm({{ $club }});event.preventDefault();">
                                         <div class="card-header card_small mb-3">
                                             {{ $club['club']['name'] }} <i class="fa fa-fw fa-angle-right"></i>
                                         </div>
@@ -21,7 +21,11 @@
                                 <form id="selected-club" action="{{ route('selectedClub') }}" method="POST"
                                       style="display: none;">
                                     @csrf
-                                    <input type="hidden" name="club_id" value="" id="club_id"/>
+                                    <input type="hidden" name="admin" id="admin"/>
+                                    <input type="hidden" name="club_id" id="club_id"/>
+                                    <input type="hidden" name="id" id="id"/>
+                                    <input type="hidden" name="user_id" id="user_id"/>
+                                    <input type="hidden" name="uuid" id="uuid"/>
                                 </form>
                             </div>
                         </div>
@@ -39,9 +43,13 @@
             ".user.user-menu li:nth-child(2), .user.user-menu li:nth-child(4), .navbar-btn.sidebar-toggle").hide();
         $(".right-side").css({"margin-left": 0});
 
-        function postForm(club_id) {
+        function postForm(club) {
             var form = $("#selected-club");
-            $("#selected-club input[name='club_id']").val(club_id);
+            $("#selected-club input[name='admin']").val(club.admin);
+            $("#selected-club input[name='club_id']").val(club.club_id);
+            $("#selected-club input[name='id']").val(club.id);
+            $("#selected-club input[name='user_id']").val(club.user_id);
+            $("#selected-club input[name='uuid']").val(club.uuid);
             form.submit();
         }
     </script>
