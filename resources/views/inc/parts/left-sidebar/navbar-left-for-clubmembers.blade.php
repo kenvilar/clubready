fc91fba2741ad6e2b694f29652f5a33fad393e14
Ken Vilar <kenvilar@gmail.com>
2020-05-11 17:50:26 +0800

    Add session()->has('my_app__current_member')

--- resources/views/inc/parts/left-sidebar/navbar-left-for-clubmembers.blade.php
+++ resources/views/inc/parts/left-sidebar/navbar-left-for-clubmembers.blade.php
@@ -14,27 +14,29 @@
         </a>
     </li>
 @endif
-<li class="{{request()->routeIs('web.club-member.memberships.*') ? 'active' : ''}}">
-    <a href="{{ route('web.club-member.memberships.index', session('my_app__current_member')['uuid']) }}">
-        <i class="menu-icon ti-tag"></i>
-        <span class="mm-text ">Memberships</span>
-    </a>
-</li>
-<li class="{{request()->routeIs('web.club-member.membership-types.*') ? 'active' : ''}}">
-    <a href="{{ route('web.club-member.membership-types.index', session('my_app__current_member')['uuid']) }}">
-        <i class="menu-icon ti-bookmark-alt"></i>
-        <span class="mm-text ">Membership Types</span>
-    </a>
-</li>
-<li class="{{request()->routeIs('web.club-member.membership-type-names.*') ? 'active' : ''}}">
-    <a href="{{ route('web.club-member.membership-type-names.index', session('my_app__current_member')['uuid']) }}">
-        <i class="menu-icon ti-id-badge"></i>
-        <span class="mm-text ">Membership Type Names</span>
-    </a>
-</li>
-<li class="{{request()->routeIs('web.club-member.vehicles.*') ? 'active' : ''}}">
-    <a href="{{ route('web.club-member.vehicles.index', session('my_app__current_member')['uuid']) }}">
-        <i class="menu-icon ti-car"></i>
-        <span class="mm-text ">Vehicles</span>
-    </a>
-</li>
+@if (session()->has('my_app__current_member'))
+    <li class="{{request()->routeIs('web.club-member.memberships.*') ? 'active' : ''}}">
+        <a href="{{ route('web.club-member.memberships.index', session('my_app__current_member')['uuid']) }}">
+            <i class="menu-icon ti-tag"></i>
+            <span class="mm-text ">Memberships</span>
+        </a>
+    </li>
+    <li class="{{request()->routeIs('web.club-member.membership-types.*') ? 'active' : ''}}">
+        <a href="{{ route('web.club-member.membership-types.index', session('my_app__current_member')['uuid']) }}">
+            <i class="menu-icon ti-bookmark-alt"></i>
+            <span class="mm-text ">Membership Types</span>
+        </a>
+    </li>
+    <li class="{{request()->routeIs('web.club-member.membership-type-names.*') ? 'active' : ''}}">
+        <a href="{{ route('web.club-member.membership-type-names.index', session('my_app__current_member')['uuid']) }}">
+            <i class="menu-icon ti-id-badge"></i>
+            <span class="mm-text ">Membership Type Names</span>
+        </a>
+    </li>
+    <li class="{{request()->routeIs('web.club-member.vehicles.*') ? 'active' : ''}}">
+        <a href="{{ route('web.club-member.vehicles.index', session('my_app__current_member')['uuid']) }}">
+            <i class="menu-icon ti-car"></i>
+            <span class="mm-text ">Vehicles</span>
+        </a>
+    </li>
+@endif
