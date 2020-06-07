2e306fb0b680c41aa3a8b0f7746f5d7f7f7f6bf7
Ken Vilar <kenvilar@gmail.com>
2020-06-07 19:12:14 +0800

    Create initial club-members.memberships CreateAndEditView.vue

---
+++ resources/js/components/member/memberships/CreateAndEditView.vue
@@ -0,0 +1,172 @@
+<template>
+    <section class="content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card striped_full">
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            <i class="ti-align-justify"></i> {{isEditView(item.id) ? 'Edit' : 'Create'}} Item
+                        </h3>
+                    </div>
+                    <div class="card-body border">
+                        <form @submit.prevent="storeOrUpdate" enctype="multipart/form-data" class="form-bordered-row"
+                              novalidate>
+                            <div class="form-group row striped-col required">
+                                <div class="col-sm-3 text-right txt_media">
+                                    <label class="form-control-label" for="name">Name</label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <input type="text" class="form-control" name="name" id="name" v-model="item.name"
+                                           :class="{'is-invalid': hasError(errors, 'name')}">
+                                    <span role="alert" class="invalid-feedback">
+                                        <strong>{{hasError(errors, 'name', true)}}</strong></span>
+                                </div>
+                            </div>
+                            <div class="form-group row required">
+                                <div class="col-sm-3 text-right txt_media">
+                                    <label class="form-control-label" for="year">Year</label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <input type="number" class="form-control" name="year" id="year" v-model="item.year"
+                                           :class="{'is-invalid': hasError(errors, 'year')}">
+                                    <span role="alert" class="invalid-feedback">
+                                        <strong>{{hasError(errors, 'year', true)}}</strong></span>
+                                </div>
+                            </div>
+                            <div class="form-group row striped-col required">
+                                <div class="col-sm-3 text-right txt_media">
+                                    <label class="form-control-label" for="amount">Amount</label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <input type="text" class="form-control" name="amount" id="amount" v-model="item.amount"
+                                           :class="{'is-invalid': hasError(errors, 'amount')}">
+                                    <span role="alert" class="invalid-feedback">
+                                        <strong>{{hasError(errors, 'amount', true)}}</strong></span>
+                                </div>
+                            </div>
+                            <div class="form-group form-actions">
+                                <div class="col-sm-9 col-sm-offset-3 ml-auto">
+                                    <button type="submit"
+                                            class="btn btn-effect-ripple btn-primary">
+                                        {{ isEditView(item.id) ? 'Edit' : 'Create'}}</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'memberships-create-and-edit-view-vue',
+        props: {
+            model_id: {
+                type: Number,
+                default: 0,
+                required: false
+            }
+        },
+        mounted() {
+            this.edit();
+        },
+        created() {
+            //
+        },
+        components: {
+            //
+        },
+        data() {
+            return {
+                database_model: 'memberships',
+                item: {},
+                errors: {},
+            }
+        },
+        methods: {
+            async storeOrUpdate() {
+                if (this.model_id) {
+                    let update = axios.put(`/api/${this.database_model}/${this.model_id}`, this.item)
+                        .then(response => {
+                            this.item = response.data;
+
+                            swal.fire({
+                                icon: 'success',
+                                title: "Success",
+                                text: "Membership has been updated successfully.",
+                                type: "success",
+                            }).then(result => {
+                                if (result.value) {
+                                    this.automaticFocusTheFirstInput();
+                                }
+                            });
+                        }, error => {
+                            this.errors = error.response.data.error;
+                            console.log('this.errors', this.errors);
+                        }).catch(err => {
+                            //
+                        });
+
+                    return;
+                }
+
+                let store = axios.post(`/api/${this.database_model}`, this.item)
+                    .then(() => {
+                        swal.fire({
+                            icon: 'success',
+                            title: "Success",
+                            text: "New membership has been created successfully.",
+                            type: "success",
+                        }).then(result => {
+                            if (result.value) {
+                                this.clearFields(this.item);
+                                this.automaticFocusTheFirstInput();
+                            }
+                        });
+                    }, error => {
+                        this.errors = error.response.data.error;
+                        console.log('this.errors', this.errors);
+                    }).catch(err => {
+                        //
+                    });
+            },
+            async edit() {
+                if (this.model_id) {
+                    let edit = axios.get(`/api/${this.database_model}/${this.model_id}`, this.item)
+                        .then(response => {
+                            this.item = response.data;
+                        }, error => {
+                            this.errors = error.response.data.error;
+                        }).catch(err => {
+                            //
+                        });
+                }
+            },
+            async clearFields(param) {
+                if (param) {
+                    this.item = {};
+                    this.errors = {};
+                }
+                return this.item;
+            },
+            isEditView(param) {
+                return typeof param !== "undefined" || param !== undefined;
+            },
+            hasError(errors, name, showResult = false) {
+                if (showResult === true) {
+                    return typeof errors[name] == 'undefined' ? (typeof errors === 'string' ? errors : '') : errors[name][0];
+                }
+
+                return typeof errors[name] == 'undefined' ? (typeof errors === 'string' ? errors : '') : errors[name];
+            },
+            automaticFocusTheFirstInput() {
+                setTimeout(() => {
+                    $('input:visible:enabled').toggleClass('focus-visible');
+                    $('form:first *:input[type!=hidden]:first').focus();
+                }, 300);
+            },
+        }
+    }
+</script>
