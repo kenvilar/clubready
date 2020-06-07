5ec9000508eaefabca2075a1827e3f9c0f4b9f06
Ken Vilar <kenvilar@gmail.com>
2020-06-07 18:47:17 +0800

    Add 404 page

---
+++ resources/views/auth/not-found.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <section class="content">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 m-auto">
+                <div class="text-center">
+
+                    <div class="error_img">
+                        <img src="img/pages/404.gif" alt="404 error image">
+                    </div>
+                    <hr class="seperator">
+                    <a href="/admin" class="btn btn-primary link-home">Go to dashboard page</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
