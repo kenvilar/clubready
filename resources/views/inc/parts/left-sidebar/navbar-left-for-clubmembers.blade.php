bc2da8cf4dbef0326674533a093e9c3447b33a0b
Ken Vilar <kenvilar@gmail.com>
2020-06-12 14:20:54 +0800

    Fix club-admin.club-members page and route

--- resources/views/inc/parts/left-sidebar/navbar-left-for-clubmembers.blade.php
+++ resources/views/inc/parts/left-sidebar/navbar-left-for-clubmembers.blade.php
@@ -1,6 +1,6 @@
-@if (UserHelper::isClubAdmin())
+@if (UserHelper::isClubAdmin() && session()->has('my_app__current_member'))
     <li class="{{request()->routeIs('web.club-member.*') ? 'active' : ''}}">
-        <a href="{{ route('web.club-member.index') }}">
+        <a href="{{ route('web.club-member.members.index', session('my_app__current_member')['uuid']) }}">
             <i class="menu-icon icon-people icons"></i>
             <span class="mm-text ">Club Members</span>
         </a>
