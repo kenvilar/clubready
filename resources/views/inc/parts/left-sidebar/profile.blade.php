40f974787dfcebad22b10fa12ab4239d6fef8531
Ken Vilar <kenvilar@gmail.com>
2020-06-19 22:06:09 +0800

    Change session('my_app__current_member')['club_id'] to session('my_app__current_member')

--- resources/views/inc/parts/left-sidebar/profile.blade.php
+++ resources/views/inc/parts/left-sidebar/profile.blade.php
@@ -3,7 +3,7 @@
         <a class="float-left profile-thumb" href="#">
             <img src="{{ asset('img/authors/avatar1.jpg') }}" class="rounded-circle" alt="User Image"></a>
         <div class="content-profile">
-            @if (session('my_app__current_member')['club_id'])
+            @if (session('my_app__current_member'))
                 <h4 class="media-heading">{{UserHelper::getClubName(session('my_app__current_member')['club_id'])}}</h4>
             @endif
             <h4 class="media-heading">
