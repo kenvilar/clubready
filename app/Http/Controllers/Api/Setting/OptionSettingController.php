8e3a8ceae687efddb18a530355894f89c5461877
Ken Vilar <kenvilar@gmail.com>
2020-07-10 06:32:59 +0800

    Delete some functions in API Setting Controller

--- app/Http/Controllers/Api/Setting/OptionSettingController.php
+++ app/Http/Controllers/Api/Setting/OptionSettingController.php
@@ -19,16 +19,6 @@ class OptionSettingController extends ApiController
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param \Illuminate\Http\Request $request
@@ -58,17 +48,6 @@ class OptionSettingController extends ApiController
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
     {
         //
     }
