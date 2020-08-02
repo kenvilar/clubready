5892f6f8cca204c3f0d2f853487d965717260074
Ken Vilar <kenvilar@gmail.com>
2020-08-03 00:13:12 +0800

    Update favicon links

--- resources/js/components/admin/settings/EditView.vue
+++ resources/js/components/admin/settings/EditView.vue
@@ -26,7 +26,7 @@
                             <div class="form-group row">
                                 <div class="col-sm-3 text-right txt_media">
                                     <label class="form-control-label">Logo <br> Recommended
-                                        resolution: <br>(width: 254px, height: 153px)</label>
+                                        resolution: <br>254x153</label>
                                 </div>
                                 <div class="col-sm-6">
                                     <input type="file" class="" name="logo" id="logo"
